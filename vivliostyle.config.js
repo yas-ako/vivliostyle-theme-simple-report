1cc2258070600eded412473fa60ac0177b135e85
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-07-03 01:31:14 +0900

    chore: 不必要な設定を削除

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -2,12 +2,4 @@ module.exports = {
   language: 'ja',
   theme: ['node_modules/@vivliostyle/theme-base', '.'],
   entry: ['example/default.md'],
-  workspaceDir: '.vivliostyle',
-  output: [
-    'book.pdf',
-    {
-      path: './book',
-      format: 'webpub',
-    },
-  ],
 };
